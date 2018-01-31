82bb300307761b9f46e056a2a264ce86ade353c7
Seth Drew <seth@sethdrew.com>
2018-01-31 14:17:48 -0500

    broken version of folding

--- gen.py
+++ gen.py
@@ -8,7 +8,7 @@ q = QuakeHeap()
 for i in range(int(sys.argv[1])):
     q.insert(i)
 
-q.extract_min()
+q.extract_min() #triggers merging the heaps so they are merged in the viz
 
 q.dump("tree_data.json")
 
